135b7bf663f800687b7c3b2af9abb72307f0ad0d
Kate <e.sheverdenok@gmail.com>
2024-04-28 13:56:27 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-### Hexlet tests and linter status:
+[![Maintainability](https://api.codeclimate.com/v1/badges/96d1e3f3eadd7583945a/maintainability)](https://codeclimate.com/github/mynameiskatherine/java-project-99/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/96d1e3f3eadd7583945a/test_coverage)](https://codeclimate.com/github/mynameiskatherine/java-project-99/test_coverage)
 [![Actions Status](https://github.com/mynameiskatherine/java-project-99/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/mynameiskatherine/java-project-99/actions)
+[![Gradle build status](https://github.com/mynameiskatherine/java-project-99/actions/workflows/my-build-checks.yml/badge.svg)](https://github.com/mynameiskatherine/java-project-99/actions)
+
+# Task manager
+
+## Description
+Task management system (similar to for example http://www.redmine.org/). It allows you to set tasks, assign executors and change their statuses. To work with the system, registration and authentication are required.
+
+*PS: This is training project. It's accessible via [this link](https://java-project-99-quwy.onrender.com).*
+
+
---
+++ app/build.gradle.kts
@@ -0,0 +1,85 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+//	java
+//	checkstyle
+	application
+	jacoco
+	id("org.springframework.boot") version "3.2.5"
+	id("io.spring.dependency-management") version "1.1.4"
+	id("io.freefair.lombok") version "8.6"
+	id("com.github.ben-manes.versions") version "0.50.0"
+}
+
+group = "hexlet.code"
+version = "0.0.1-SNAPSHOT"
+
+application {
+	mainClass.set("hexlet.code.AppApplication")
+}
+
+tasks.jar {
+	manifest {
+		attributes(
+				"Main-Class" to application.mainClass,
+				"Class-Path" to "/libs/*.jar"
+		)
+	}
+}
+
+//java {
+//	sourceCompatibility = JavaVersion.VERSION_21
+//}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-devtools")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-configuration-processor")
+	//implementation("org.springframework.boot:spring-boot-starter-security")
+	//implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+
+	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+	implementation("org.mapstruct:mapstruct:1.6.0.Beta1")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.6.0.Beta1")
+
+	// implementation("io.github.wimdeblauwe:error-handling-spring-boot-starter:4.2.0")
+
+	implementation("org.instancio:instancio-junit:3.6.0")
+	implementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+	implementation("net.datafaker:datafaker:2.0.2")
+
+	runtimeOnly("com.h2database:h2:2.2.224")
+	//testImplementation("org.springframework.security:spring-security-test")
+	testImplementation(platform("org.junit:junit-bom:5.10.1"))
+	testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+	testLogging {
+		exceptionFormat = TestExceptionFormat.FULL
+		events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+		showStackTraces = true
+		showCauses = true
+		showStandardStreams = true
+	}
+	finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+	reports {
+		xml.required = true
+	}
+}
