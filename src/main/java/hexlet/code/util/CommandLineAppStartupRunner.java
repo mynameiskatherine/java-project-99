aa44e690d1bb07ab5f4282b55cce30d0c32d301f
Kate <e.sheverdenok@gmail.com>
2024-08-29 09:19:18 +0000

    added checks of existence for initial admins

--- src/main/java/hexlet/code/util/CommandLineAppStartupRunner.java
+++ src/main/java/hexlet/code/util/CommandLineAppStartupRunner.java
@@ -29,10 +29,27 @@ public class CommandLineAppStartupRunner implements CommandLineRunner {
 
         User user = new User("hexlet@example.com",
                 passwordEncoder.encode("qwerty"));
-        userRepository.save(user);
+        if (!userRepository.existsByEmail(user.getEmail())) {
+            userRepository.save(user);
+        } else {
+            User existedAdmin = userRepository.findByEmail(user.getEmail()).get();
+            if (!(existedAdmin.getPassword().equals(user.getPassword()))) {
+                existedAdmin.setPassword(user.getPassword());
+                userRepository.save(existedAdmin);
+            }
+        }
+
         User user2 = new User("test@test.com",
                 passwordEncoder.encode("testtest"));
-        userRepository.save(user2);
+        if (!userRepository.existsByEmail(user2.getEmail())) {
+            userRepository.save(user2);
+        } else {
+            User existedAdmin = userRepository.findByEmail(user2.getEmail()).get();
+            if (!(existedAdmin.getPassword().equals(user2.getPassword()))) {
+                existedAdmin.setPassword(user2.getPassword());
+                userRepository.save(existedAdmin);
+            }
+        }
 
         taskStatusRepository.save(new TaskStatus("draft", "draft"));
         taskStatusRepository.save(new TaskStatus("to_review", "ToReview"));
